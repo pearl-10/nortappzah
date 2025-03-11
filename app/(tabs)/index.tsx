9d83edf3276b66f8dce05113aefdafc4eaef7229
0727068 <0727068@gmail.com>
2025-03-11 16:12:26 +0000

    videocomponent started

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import AuthOverlay from '@/components/overlay/AuthOverlay';
+import VideoComponent from '@/components/VideoComponent';
 import { StyleSheet, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <AuthOverlay />
+      <VideoComponent />
     </SafeAreaView>
   
   );
